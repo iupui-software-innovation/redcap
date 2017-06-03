f654c80e6efa20176a2e92cb140b8b1931027e5a
Alan Unger <ajunger0@gmail.com>
2017-06-03 16:36:57 -0400

    Implemented project exportinfo as example

---
+++ test/test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Hello I am not a proper unit test yet
+// This is just to make sure stuff even works
+var redcap = require('../lib/redcap.js');
+
+var params = {};
+var callback = function(data) {
+	console.log(data);
+};
+
+redcap.project.exportInfo(params, callback);
