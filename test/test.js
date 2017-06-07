1aa431bf381e892e298cb462f60f3636bf502acb
Evan <evdawilt@umail.iu.edu>
2017-06-06 20:57:13 -0400

    Updated file structure, added mocha test

--- test/test.js
+++ test/test.js
@@ -1,12 +1,21 @@
+
 'use strict';
 
 // Hello I am not a proper unit test yet
 // This is just to make sure stuff even works
-var redcap = require('../lib/redcap.js');
+var config = require('../lib/config.json');
+var redcap = require('../lib/redcap.js')(config.token, config);
+var assert = require('assert');
 
 var params = {};
-var callback = function(data) {
-	console.log(data);
+
+var callback = function(data, err) {
+	if (err) done(err);
+	else done();
 };
 
-redcap.users.exportInfo(params, callback);
+describe('Users test', function(){
+	it('Should output all users', function() {
+		redcap.users.exportInfo(params, callback);
+	});
+});
