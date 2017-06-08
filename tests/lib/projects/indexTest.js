c31a4a5997a33f4b67e51ceb8107478c8ad676ef
Evan <evdawilt@umail.iu.edu>
2017-06-08 13:10:27 -0400

    Finished adding Alan's tests

--- tests/lib/projects/indexTest.js
+++ tests/lib/projects/indexTest.js
@@ -1,29 +1,29 @@
 'use strict';
 
 const expect = require('chai').expect;
-const usersModule = require('../../../lib/users');
+const metadataModule = require('../../../lib/metadata');
 const utils = require('../../../lib/utils');
 
-context('users', function() {
+context('metadata', function() {
 	describe('module.export', function() {
 		it('should be a function', function() {
-			expect(usersModule).to.be.a('function');
+			expect(metadataModule).to.be.a('function');
 		});
 
 		describe('constructor', function() {
 			it('should return an object', function() {
-				expect(usersModule(utils)).to.be.an('object');
+				expect(metadataModule(utils)).to.be.an('object');
 			});
 		});
 
 		describe('#export', function() {
 			it('should be a function', function() {
-				expect(usersModule(utils).export).to.be.a('function');
+				expect(metadataModule(utils).export).to.be.a('function');
 			});
 		});
 		describe('#import', function() {
 			it('should be a function', function() {
-				expect(usersModule(utils).import).to.be.a('function');
+				expect(metadataModule(utils).import).to.be.a('function');
 			});
 		});
 	});
