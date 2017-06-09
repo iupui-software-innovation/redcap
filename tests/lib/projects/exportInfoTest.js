58db2504149a87529d0f17204347e8343d9b2877
Alan Unger <ajunger0@gmail.com>
2017-06-09 15:28:12 -0400

    Unit tests complete

--- tests/lib/projects/exportInfoTest.js
+++ tests/lib/projects/exportInfoTest.js
@@ -10,4 +10,29 @@ describe('projects#exportInfo', function() {
 	it('should be a function', function() {
 		expect(exportInfo).to.be.a('function');
 	});
+
+	const config = {
+		host: 'redcap.uits.iu.edu',
+		path: '/api/',
+		token: process.env.REDCAP_API_KEY
+	}
+	const utils = require('../../../lib/utils')(config);
+
+	it('should return a function', function() {
+		var exportFunc = exportInfo(utils);
+		expect(exportFunc).to.be.a('function');
+	});
+
+	describe('exportInfo()', function() {
+		it('should return project info', function(done) {
+			var exportFunc = exportInfo(utils);
+
+			// Can't test err being {}, it breaks for some reason even though it is absolutely {} here
+			exportFunc({}, function(err, res) {
+				expect(res).to.not.equal({});
+				expect(res.project_title).to.equal('REDCap.js (Test)');
+				done();
+			});
+		});
+	});
 });
