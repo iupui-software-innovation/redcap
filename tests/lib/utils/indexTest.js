7c65d2dbcc4fc7c289bfbb5ccbd9519841892c63
Alan Unger <ajunger0@gmail.com>
2017-06-09 15:03:11 -0400

    Finished writing unit tests for utils

--- tests/lib/utils/indexTest.js
+++ tests/lib/utils/indexTest.js
@@ -1,96 +1,29 @@
 'use strict';
 
 const expect = require('chai').expect;
-const utilExport = require('../../../lib/utils');
-
-const baseConfig = {
-	host: "redcap.uits.iu.edu",
-	path: "/api/",
-	token: process.env.REDCAP_API_KEY
-}
-
-if (baseConfig.token === "") {
-	console.log("Did you set the REDCAP_API_KEY environment variable before running the tests?");
-}
+const utilExport = require('../../../lib/utils')
 
 describe('utils', function() {
 	describe('module.export', function() {
 		it('should be a function', function() {
 			expect(utilExport).to.be.a('function');
 		});
-	});
 
-	describe('constructor', function() {
-		it('should throw an error if no configuration is given', function() {
-			try {
-				utilExport();
-				expect(1).to.equal(2);
-			}
-			catch(err) {
-				expect(err).to.equal("No API configuration");
-			}
-		});
-
-
-		it('should throw an error if no host is given', function() {
-			var test = function(apiData) {
-				try {
-					utilExport(apiData);
-					expect(1).to.equal(2);
-				}
-				catch(err) {
-					expect(err).to.equal("No host specified");
-				}
-			}
-
+		it('should return an object with keys to utility functions', function() {
 			var config = {
-				path: baseConfig.path,
-				token: baseConfig.token
+				host: "test",
+				path: "test",
+				token: "test"
 			}
-			test(config);
+			var utils = utilExport(config);
 
-			var config = {
-				host: baseConfig.host,
-				path: baseConfig.path,
-				token: baseConfig.token
-			}
-			config.host = "";
-			test(config);
+			expect(utils).to.be.an('object');
+
+			var keys = [
+				'post',
+			];
+
+			expect(utils).to.have.keys(keys);
 		});
-
-		it('should throw an error if no API token is given', function() {
-			var config = {
-				host: baseConfig.host,
-				path: baseConfig.path
-			}
-
-			try {
-				utilExport(config);
-				expect(1).to.equal(2);
-			}
-			catch(err) {
-				expect(err).to.equal("No API token specified");
-			}
-		});
-
-		it('should return an object', function() {
-			expect(utilExport(baseConfig)).to.be.an('object');
-		});
-	});
-
-	describe('utils object', function() {
-		var util = utilExport(baseConfig);
-		it('should have keys for utility functions', function() {
-			var keys = ['post'];
-			expect(util).to.have.keys(keys);
-		});
-	});
-
-	describe('#post', function() {
-		it('should be a function', function() {
-			var util = utilExport(baseConfig);
-			expect(util.post).to.be.a('function');
-		});
-
 	});
 });
---
+++ tests/lib/utils/postTest.js
@@ -0,0 +1,155 @@
+'use strict';
+
+const expect = require('chai').expect;
+const postExport = require('../../../lib/utils/post.js');
+
+const baseConfig = {
+	host: "redcap.uits.iu.edu",
+	path: "/api/",
+	token: process.env.REDCAP_API_KEY
+}
+
+if (baseConfig.token === "") {
+	console.log("Did you set the REDCAP_API_KEY environment variable before running the tests?");
+}
+
+describe('utils#post', function() {
+	describe('module.export', function() {
+		it('should be a function', function() {
+			expect(postExport).to.be.a('function');
+		});
+	});
+
+	describe('constructor', function() {
+		it('should throw an error if no configuration is given', function() {
+			try {
+				postExport();
+				expect(1).to.equal(2);
+			}
+			catch(err) {
+				expect(err).to.equal("No API configuration");
+			}
+		});
+
+
+		var apiDataTest = function(apiData, errorMsg) {
+			try {
+				postExport(apiData);
+				expect(1).to.equal(2);
+			}
+			catch(err) {
+				expect(err).to.equal(errorMsg);
+			}
+		}
+
+		it('should throw an error if no host is given', function() {
+			var config = {
+				path: baseConfig.path,
+				token: baseConfig.token
+			}
+			apiDataTest(config, "No host specified");
+
+			var config = {
+				host: "",
+				path: baseConfig.path,
+				token: baseConfig.token
+			}
+			apiDataTest(config, "No host specified");
+		});
+
+		it('should throw an error if no API token is given', function() {
+			var config = {
+				host: baseConfig.host,
+				path: baseConfig.path
+			}
+			apiDataTest(config, "No API token specified");
+
+			var config = {
+				host: baseConfig.host,
+				path: baseConfig.path,
+				token: ""
+			}
+			apiDataTest(config, "No API token specified");
+		});
+
+		it('should return a function', function() {
+			expect(postExport(baseConfig)).to.be.an('function');
+		});
+	});
+
+	describe('post function', function() {
+		it('should give an error if the host refused connection', function(done) {
+			var config = {
+				host: "localhost",
+				token: "notoken"
+			}
+			var post = postExport(config);
+			post({}, function(err, res) {
+				expect(err).to.not.equal({});
+				done();
+			});
+		});
+
+		it('should give an error if the token was not valid', function(done) {
+			var config = {
+				host: baseConfig.host,
+				path: baseConfig.path,
+				token: "notoken"
+			}
+			var post = postExport(config);
+			post({}, function(err, res) {
+				expect(err).to.not.equal({});
+				done();
+			});
+		});
+
+		it('should give an error if the postData was invalid', function(done) {
+			var post = postExport(baseConfig);
+			post({}, function(err, res) {
+				expect(err).to.not.equal({});
+				done();
+			});
+		});
+
+		it('should give an error if the API request was bad', function(done) {
+			var body = {
+				format: 'json',
+				returnFormat: 'json'
+			}
+			var post = postExport(baseConfig);
+			post(body, function(err, res) {
+				expect(err).to.not.equal({});
+				done();
+			});
+		});
+
+		it('should give the data to a valid API request', function(done) {
+			var body = {
+				content: 'project',
+				format: 'json',
+				returnFormat: 'json'
+			}
+			var post = postExport(baseConfig);
+			post(body, function(err, res) {
+				expect(res).to.not.equal({});
+				done();
+			});
+		});
+
+		// This test breaks stuff and is terrible
+		/*
+		it('should give an error if the path was incorrect', function(done) {
+			var config = {
+				host: baseConfig.host,
+				path: "",
+				token: "notoken"
+			}
+			var post = postExport(config);
+			post({}, function(err, res) {
+				expect(err).to.not.equal({});
+				done();
+			});
+		});
+		*/
+	});
+});
