59498483af54fab9c8a696815e82f32e38428955
Alan Unger <ajunger0@gmail.com>
2017-06-09 18:12:00 -0400

    Implemented generateNextRecordName

--- tests/lib/projects/exportInfoTest.js
+++ tests/lib/projects/exportInfoTest.js
@@ -3,9 +3,6 @@
 const expect = require('chai').expect;
 const exportInfo = require('../../../lib/projects/exportInfo.js');
 
-// Temporary configuration import - is there a better way?
-const baseConfig = require('../config.json');
-
 describe('projects#exportInfo', function() {
 	it('should be a function', function() {
 		expect(exportInfo).to.be.a('function');
@@ -23,16 +20,14 @@ describe('projects#exportInfo', function() {
 		expect(exportFunc).to.be.a('function');
 	});
 
-	describe('exportInfo()', function() {
-		it('should return project info', function(done) {
-			var exportFunc = exportInfo(utils);
+	it('should return project info', function(done) {
+		var exportFunc = exportInfo(utils);
 
-			// Can't test err being {}, it breaks for some reason even though it is absolutely {} here
-			exportFunc({}, function(err, res) {
-				expect(res).to.not.equal({});
-				expect(res.project_title).to.equal('REDCap.js (Test)');
-				done();
-			});
+		// Can't test err being {}, it breaks for some reason even though it is absolutely {} here
+		exportFunc({}, function(err, res) {
+			expect(res).to.not.equal({});
+			expect(res.project_title).to.equal('REDCap.js (Test)');
+			done();
 		});
 	});
 });
