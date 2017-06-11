90d1b4ce78b2d340ebf37bae083f8c25ff5e0a69
Alan Unger <ajunger0@gmail.com>
2017-06-11 17:45:10 -0400

    Finished export records

---
+++ tests/lib/records/exportTest.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const expect = require('chai').expect;
+const config = {
+	host: 'redcap.uits.iu.edu',
+	path: '/api/',
+	token: process.env.REDCAP_API_KEY
+}
+
+const utils = require('../../../lib/utils')(config);
+
+const exportModule = require('../../../lib/records/export.js');
+
+describe('records#export', function() {
+	it('should be a function', function() {
+		expect(exportModule).to.be.a('function');
+	});
+
+	var exportRecord = exportModule(utils);
+	it('should return a function', function() {
+		expect(exportRecord).to.be.a('function');
+	});
+
+	it('should generate an error if "type" is missing from parameters', function(done) {
+		var params = {};
+
+		exportRecord(params, function(err, res) {
+			expect(err).to.be.an('object').that.has.property('error');
+			done();
+		});
+	});
+
+	it('should return all records if no options are given?', function(done) {
+		var params = {type: 'flat'};
+
+		exportRecord(params, function(err, res) {
+			expect(err).to.be.an('object').that.is.empty;
+			expect(res).to.be.an('array').that.is.not.empty;
+
+			done();
+		});
+	});
+
+	it('should accept optional parameters', function(done) {
+		var params = {
+			type: 'eav',
+			records: ['1']
+		}
+
+		exportRecord(params, function(err, res) {
+			expect(err).to.be.an('object').that.is.empty;
+			expect(res).to.be.an('array').that.is.not.empty;
+			expect(res[0]).to.be.an('object').that.has.property('record').that.equals('1');
+
+			done();
+		});
+	});
+	it('should ignore invalid parameters', function(done) {
+		var params = {
+			type: 'flat',
+			foo: 'bar'
+		}
+
+		exportRecord(params, function(err, res) {
+			expect(err).to.be.an('object').that.is.empty;
+			expect(res).to.be.an('array').that.is.not.empty;
+
+			done();
+		});
+	});
+});
