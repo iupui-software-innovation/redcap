7fcc09c1e66ad7a9b361483f79f26b204b96c33a
Alan Unger <ajunger0@gmail.com>
2017-06-11 17:45:10 -0400

    Finished export records

--- tests/lib/records/indexTest.js
+++ tests/lib/records/indexTest.js
@@ -8,4 +8,13 @@ describe('records', function() {
 	it('should be a function', function() {
 		expect(recordsModule).to.be.a('function');
 	});
+
+	var keys = [
+		'exportRecords'
+	];
+
+	var records = recordsModule(utils);
+	it('should return an object with keys', function() {
+		expect(records).to.be.an('object').that.has.keys(keys);
+	});
 });
