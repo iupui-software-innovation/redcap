9c39cf26b85b988f2bf351ba6eef33de3642405c
Evan <evdawilt@umail.iu.edu>
2017-06-12 12:58:26 -0400

    Added warning for import test

--- tests/lib/metadata/importTest.js
+++ tests/lib/metadata/importTest.js
@@ -1,51 +1,53 @@
-'use strict';
+// Do not use this test unless you are 100% AWARE of the consequences.
 
-const expect = require('chai').expect;
-const importInfoModule = require('../../../lib/metadata/import.js');
+// 'use strict';
 
-const config = require('../config.json');
-const utils = require('../../../lib/utils')(config);
+// const expect = require('chai').expect;
+// const importInfoModule = require('../../../lib/metadata/import.js');
 
-describe('metadata#importInfo', function() {
-	it('should be a function', function() {
-		expect(importInfoModule).to.be.a('function');
-	});
+// const config = require('../config.json');
+// const utils = require('../../../lib/utils')(config);
 
-	const importInfo = importInfoModule(utils);
+// describe('metadata#importInfo', function() {
+// 	it('should be a function', function() {
+// 		expect(importInfoModule).to.be.a('function');
+// 	});
 
-	it('should return a function', function() {
-		expect(importInfo).to.be.a('function');
-	});
+// 	const importInfo = importInfoModule(utils);
 
-	it('should provide an error if no values are passed', function(done) {
-		importInfo({data:{}}, function(err, res) {
-			expect(err).to.not.be.empty;
-			expect(res).to.be.empty;
-			done();
-		});
-	});
+// 	it('should return a function', function() {
+// 		expect(importInfo).to.be.a('function');
+// 	});
 
-	describe('should return the number of values updated', function() {
-		it('for 1 update', function(done) {
-			var data = { 
-				// TODO Add test metadata
-			}
-			importInfo({data: JSON.stringify(data)}, function(err, res) {
-				expect(err).to.be.empty;
-				expect(res).to.equal(1);
-				done();
-			});
-		});
+// 	it('should provide an error if no values are passed', function(done) {
+// 		importInfo({data:{}}, function(err, res) {
+// 			expect(err).to.not.be.empty;
+// 			expect(res).to.be.empty;
+// 			done();
+// 		});
+// 	});
 
-		it('for 2 updates', function(done) {
-			var data = {
-				// TODO Add test metadata
-			}
-			importInfo({data: JSON.stringify(data)}, function(err, res) {
-				expect(err).to.be.empty;
-				expect(res).to.equal(2);
-				done();
-			});
-		});
-	});
-});
+// 	describe('should return the number of values updated', function() {
+// 		it('for 1 update', function(done) {
+// 			var data = { 
+// 				// TODO Add test metadata
+// 			}
+// 			importInfo({data: JSON.stringify(data)}, function(err, res) {
+// 				expect(err).to.be.empty;
+// 				expect(res).to.equal(1);
+// 				done();
+// 			});
+// 		});
+
+// 		it('for 2 updates', function(done) {
+// 			var data = {
+// 				// TODO Add test metadata
+// 			}
+// 			importInfo({data: JSON.stringify(data)}, function(err, res) {
+// 				expect(err).to.be.empty;
+// 				expect(res).to.equal(2);
+// 				done();
+// 			});
+// 		});
+// 	});
+// });
