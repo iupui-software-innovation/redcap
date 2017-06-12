a71ff1a8cb40ba2e22bc2fae28eb952ca8335eaa
Evan <evdawilt@umail.iu.edu>
2017-06-12 18:31:27 -0400

    Added new tests, all working except import

--- tests/lib/users/importTest.js
+++ tests/lib/users/importTest.js
@@ -1,13 +1,1 @@
-'use strict';
-
-const expect = require('chai').expect;
-const importInfo = require('../../../lib/users/import.js');
-
-// Temporary configuration import - is there a better way?
-const baseConfig = require('../config.json');
-
-describe('projects#importInfo', function() {
-	it('should be a function', function() {
-		expect(importInfo).to.be.a('function');
-	});
-});
+// TODO Get user edit rights to test import test
