d9ca2a39c3b055f9cdb7957fd1bd294d75e7d1f6
Alan Unger <ajunger0@gmail.com>
2017-06-12 16:37:42 -0400

    Fixed unnecessary dependency on file that was deleted

--- tests/lib/projects/exportInfoTest.js
+++ tests/lib/projects/exportInfoTest.js
@@ -3,9 +3,6 @@
 const expect = require('chai').expect;
 const exportInfo = require('../../../lib/projects/exportInfo.js');
 
-// Temporary configuration import - is there a better way?
-const baseConfig = require('../config.json');
-
 describe('projects#exportInfo', function() {
 	it('should be a function', function() {
 		expect(exportInfo).to.be.a('function');
