060eedf772caef49027c4fc9d973fd3a03555cdc
Alan Unger <ajunger0@gmail.com>
2017-06-15 18:03:31 -0400

    Implemented export field names list method

---
+++ tests/lib/fieldNames/exportListTest.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const expect = require('chai').expect;
+const exportModule = require('../../../lib/fieldNames/exportList.js');
+const config = {
+	host: 'redcap.uits.iu.edu',
+	path: '/api/',
+	token: process.env.REDCAP_API_KEY
+}
+const utils = require('../../../lib/utils')(config);
+
+describe('fieldNames#exportList', function() {
+	it('should be a function', function() {
+		expect(exportModule).to.be.a('function');
+	});
+
+	var exportList = exportModule(utils);
+	it('should return a function', function() {
+		expect(exportList).to.be.a('function');
+	});
+
+	it('should return a list of export/import field names', function(done) {
+		exportList({}, function(error, res) {
+			expect(error).to.be.null;
+			expect(res).to.be.an('array');
+			done();
+		});
+	});
+
+	it('should return a single name if a field name is given', function(done) {
+		exportList({field: 'record_id'}, function(error, res) {
+			expect(error).to.be.null;
+			expect(res).to.be.an('array');
+			expect(res.length).to.equal(1);
+			done();
+		});
+	});
+});
