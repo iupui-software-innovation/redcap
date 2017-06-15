9d51059f758690ef571a20e49a1db48d3056b1dc
Alan Unger <ajunger0@gmail.com>
2017-06-15 16:36:03 -0400

    Implemented Export Reports

--- tests/lib/reports/exportTest.js
+++ tests/lib/reports/exportTest.js
@@ -14,4 +14,26 @@ describe('reports#exportReports', function() {
 	it('should be a function', function() {
 		expect(exportModule).to.be.a('function');
 	});
+
+	var exportReports = exportModule(utils);
+
+	it('should return a function', function() {
+		expect(exportReports).to.be.a('function');
+	});
+
+	it('should give an error if "report_id" is missing', function(done) {
+		exportReports({}, function(error, res) {
+			expect(error).to.be.an('object').that.has.property('error');
+			expect(res).to.be.null;
+			done();
+		});
+	});
+
+	it('should return reports', function(done) {
+		exportReports({report_id: 20794}, function(error, res) {
+			expect(error).to.be.null;
+			expect(res).to.not.be.null;
+			done();
+		});
+	});
 });
