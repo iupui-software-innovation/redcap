0632c83851498fc97ee9e594153e11e0d98b923e
Evan <evdawilt@umail.iu.edu>
2017-06-16 10:49:16 -0400

    Changed naming to camelcase, made version save to string, all tests working

---
+++ tests/lib/indexTest.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+const redcapModule = require('../../lib');
+
+describe('redcap.js', function() {
+	it('should be a function', function() {
+		expect(redcapModule).to.be.a('function');
+	});
+
+	it('should throw an error if token is empty', function() {
+		try {
+			redcapModule("", {});
+			expect.fail;
+		}
+		catch (err) {
+			expect(err).to.be.a('string').that.equals("No API token specified");
+		}
+	});
+
+	it('should throw an error if token is undefined', function() {
+		try {
+			redcapModule();
+			expect.fail;
+		}
+		catch (err) {
+			expect(err).to.be.a('string').that.equals("No API token specified");
+		}
+	});
+
+	it('should throw an error if options is undefined', function() {
+		try {
+			redcapModule("hi");
+			expect.fail;
+		}
+		catch (err) {
+			expect(err).to.be.a('string').that.equals("No options provided");
+		}
+	});
+
+	it('should throw an error if options.host is undefined', function() {
+		try {
+			redcapModule("hi", {});
+			expect.fail;
+		}
+		catch (err) {
+			expect(err).to.be.a('string').that.equals("Host undefined (options.host)");
+		}
+	});
+
+	it('should throw an error if options.path is undefined', function() {
+		try {
+			redcapModule("hi", {host:"localhost"});
+			expect.fail;
+		}
+		catch (err) {
+			expect(err).to.be.a('string').that.equals("Path undefined (options.path)");
+		}
+	});
+
+	var redcap = redcapModule("hi", {host:"test", path:"test"});
+	it('should return an object with keys', function() {
+		var keys = [
+			'PACKAGE_VERSION',
+			'redcapVersion'
+		];
+		expect(redcap).to.be.an('object').that.has.keys(keys);
+	});
+
+	describe('redcap#redcapVersion', function() {
+		it('should be an object', function() {
+			expect(redcap.redcapVersion).to.be.an('object');
+		});
+	});
+});
