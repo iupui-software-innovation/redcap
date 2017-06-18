7a4ed56d431189b28d992ed9f0ac257a1603f192
Evan Wilt <evdawilt@umail.iu.edu>
2017-06-17 21:48:27 -0400

    Started import and delete tests

--- tests/lib/files/importTest.js
+++ tests/lib/files/importTest.js
@@ -1,33 +1,39 @@
-// 'use strict';
+'use strict';
 
-// const expect = require('chai').expect;
-// const importInfo = require('../../../lib/files/import.js');
+const expect = require('chai').expect;
+const importInfo = require('../../../lib/files/import.js');
 
-// describe('files#import', function() {
-// 	it('should be a function', function() {
-// 		expect(importInfo).to.be.a('function');
-// 	});
+describe('files#import', function() {
+	it('should be a function', function() {
+		expect(importInfo).to.be.a('function');
+	});
 
-// 	const config = {
-// 		host: 'redcap.uits.iu.edu',
-// 		path: '/api/',
-// 		token: process.env.REDCAP_API_KEY
-// 	}
-// 	const utils = require('../../../lib/utils')(config);
+	const config = {
+		host: 'redcap.uits.iu.edu',
+		path: '/api/',
+		token: process.env.REDCAP_API_KEY
+	}
+	const utils = require('../../../lib/utils')(config);
 
-// 	it('should return a function', function() {
-// 		var importFunc = importInfo(utils);
-// 		expect(importFunc).to.be.a('function');
-// 	});
+	it('should return a function', function() {
+		var importFunc = importInfo(utils);
+		expect(importFunc).to.be.a('function');
+	});
 
-// 	it('should import a file', function(done) {
-// 		var importFunc = importInfo(utils);
+	it('should import a file', function(done) {
+		var importFunc = importInfo(utils);
 
-// 		importFunc({}, function(err, res) {
-//             console.log(res);
-// 			expect(err).to.be.null;
-// 			expect(res).to.not.be.empty;
-// 			done();
-// 		});
-// 	});
-// });
+        var params = {
+            record: '1',
+            field: 'testfile',
+            event: '',
+            file: ''
+        }
+		importFunc(params, function(err, res) {
+            console.log(res);
+			expect(err).to.be.null;
+			expect(res).to.not.be.empty;
+			done();
+		});
+	});
+});
