941a01ed67140a8e12990c9314248bced581ae69
Evan <evdawilt@umail.iu.edu>
2017-06-20 15:15:22 -0400

    Added directory parameter to export, updated post util

--- tests/lib/files/exportTest.js
+++ tests/lib/files/exportTest.js
@@ -1,37 +1,38 @@
-'use strict';
+// 'use strict';
 
-const expect = require('chai').expect;
-const exportInfo = require('../../../lib/files/export.js');
+// const expect = require('chai').expect;
+// const exportInfo = require('../../../lib/files/export.js');
 
-describe('files#export', function() {
-	it('should be a function', function() {
-		expect(exportInfo).to.be.a('function');
-	});
+// describe('files#export', function() {
+// 	it('should be a function', function() {
+// 		expect(exportInfo).to.be.a('function');
+// 	});
 
-	const config = {
-		host: 'redcap.uits.iu.edu',
-		path: '/api/',
-		token: process.env.REDCAP_API_KEY
-	}
-	const utils = require('../../../lib/utils')(config);
+// 	const config = {
+// 		host: 'redcap.uits.iu.edu',
+// 		path: '/api/',
+// 		token: process.env.REDCAP_API_KEY
+// 	}
+// 	const utils = require('../../../lib/utils')(config);
 
-	it('should return a function', function() {
-		var exportFunc = exportInfo(utils);
-		expect(exportFunc).to.be.a('function');
-	});
+// 	it('should return a function', function() {
+// 		var exportFunc = exportInfo(utils);
+// 		expect(exportFunc).to.be.a('function');
+// 	});
 
-	it('should return a file', function(done) {
-		var exportFunc = exportInfo(utils);
+// 	it('should return a file', function(done) {
+// 		var exportFunc = exportInfo(utils);
 
-        var params = {
-            record: '1',
-            field: 'testfile',
-            event: ''
-        }
-		exportFunc(params, function(err, res) {
-			expect(err).to.be.null;
-			expect(res).to.not.be.null;
-			done();
-		});
-	});
-});
+//         var params = {
+//             record: '1',
+//             field: 'testfile',
+//             event: '',
+// 			directory: ''
+//         }
+// 		exportFunc(params, function(err, res) {
+// 			expect(err).to.be.null;
+// 			expect(res).to.not.be.null;
+// 			done();
+// 		});
+// 	});
+// });
